bfd316a7aa0e8260ad78104d0cbd463ab76dc9e9
JH <modoo.jhy@gmail.com>
2026-07-24 15:44:06 +0900

    2026-07-24 15:44:06

--- lib/workout_list_page.dart
+++ lib/workout_list_page.dart
@@ -30,8 +30,46 @@ class WorkoutListPage extends StatelessWidget {
     'shoulder_stretch.png',
     'hamstring_stretch.png',
   ];
-  
+
   List<int> workoutMinutes = [30, 20, 15, 15, 20, 30, 20, 15, 25, 15, 10];
+
+  List<Row> getWorkoutList() {
+
+    for(int i=0;i< workoutName.length; i++){
+      var name=workoutName[i];
+      var minutes=workoutMinutes[i];
+      var image=workoutImage[i];
+
+
+    }
+    //###############################
+    Row sample=Row(
+      spacing: 20,
+      children: [
+        Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/squat.png'),
+              fit: BoxFit.cover,
+            ),
+            shape: BoxShape.circle,
+          ),
+        ),
+        Expanded(
+          child: Text(
+            '1.스쿼트', style: TextStyle(fontSize:20),
+          ),
+        ),
+        Text(
+          '5:30', style: TextStyle(fontSize:20, color:Colors.blue),
+        ),
+      ],
+    );
+    return [sample,sample,sample];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,83 +78,10 @@ class WorkoutListPage extends StatelessWidget {
         centerTitle: true,
       ),
       body: Column(
-        children: [
-          Row(
-            spacing: 20,
-            children: [
-              //Image.asset('assets/squat.png'),
-              Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/squat.png'),
-                    fit: BoxFit.cover,
-                  ),
-                  shape: BoxShape.circle,
-                ),
-              ),
-              Expanded(
-                child: Text(
-                  '1.스쿼트', style: TextStyle(fontSize:20),
-                ),
-              ),
-              Text(
-                '5:30', style: TextStyle(fontSize:20, color:Colors.blue),
-              ),
-            ],
-          ),
-          Row(
-            spacing:20,
-            children: [
-              //Image.asset('assets/squat.png'),
-              Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/mountain_climber.png'),
-                    fit: BoxFit.cover,
-                  ),
-                  shape: BoxShape.circle,
-                ),
-              ),
-              Expanded(
-                child: Text(
-                  '2.마운틴클림버', style: TextStyle(fontSize:20),
-                ),
-              ),
-              Text(
-                '20:30', style: TextStyle(fontSize:20, color:Colors.blue),
-              ),
-            ],
-          ),
-          Row(
-            spacing:20,
-            children: [
-              Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/push_up.png'),
-                    fit: BoxFit.cover,
-                  ),
-                  shape: BoxShape.circle,
-                ),
-              ),
-              Expanded(
-                child: Text(
-                  '3.푸쉬업', style: TextStyle(fontSize:20),
-                ),
-              ),
-              Text(
-                '12', style: TextStyle(fontSize:20, color:Colors.blue),
-              ),
-            ],
-          ),
-        ],
+        children:getWorkoutList(),
       ),
     );
   }
+
+
 }
