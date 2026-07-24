eff8fb9cf752d61dbb1483dcdda239f88e7a89a2
JH <modoo.jhy@gmail.com>
2026-07-24 14:57:59 +0900

    2026-07-24 14:57:59

--- lib/workout_list_page.dart
+++ lib/workout_list_page.dart
@@ -14,6 +14,7 @@ class WorkoutListPage extends StatelessWidget {
       body: Column(
         children: [
           Row(
+            spacing: 20,
             children: [
               //Image.asset('assets/squat.png'),
               Container(
@@ -27,15 +28,65 @@ class WorkoutListPage extends StatelessWidget {
                   shape: BoxShape.circle,
                 ),
               ),
-              Text(
-                '1.스쿼트', style: TextStyle(fontSize:20),
+              Expanded(
+                child: Text(
+                  '1.스쿼트', style: TextStyle(fontSize:20),
+                ),
               ),
               Text(
                 '5:30', style: TextStyle(fontSize:20, color:Colors.blue),
               ),
             ],
           ),
-
+          Row(
+            spacing:20,
+            children: [
+              //Image.asset('assets/squat.png'),
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/mountain_climber.png'),
+                    fit: BoxFit.cover,
+                  ),
+                  shape: BoxShape.circle,
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  '2.마운틴클림버', style: TextStyle(fontSize:20),
+                ),
+              ),
+              Text(
+                '20:30', style: TextStyle(fontSize:20, color:Colors.blue),
+              ),
+            ],
+          ),
+          Row(
+            spacing:20,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/push_up.png'),
+                    fit: BoxFit.cover,
+                  ),
+                  shape: BoxShape.circle,
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  '3.푸쉬업', style: TextStyle(fontSize:20),
+                ),
+              ),
+              Text(
+                '12', style: TextStyle(fontSize:20, color:Colors.blue),
+              ),
+            ],
+          ),
         ],
       ),
     );
