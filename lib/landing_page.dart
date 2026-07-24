6f1ba2605f2c654b00c9f8ee9e0e78994aaa8ddc
JH <modoo.jhy@gmail.com>
2026-07-24 13:36:25 +0900

    2026-07-24 13:36:24

---
+++ lib/landing_page.dart
@@ -0,0 +1,60 @@
+
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                color: Colors.black,
+                child: Text(
+                  'My perfect workout mate',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Pretendard',
+                  ),
+                ),
+              ),
+              Image(
+                image: AssetImage('assets/runner.png'),
+              ),
+              Text(
+                'Workout \nTracker',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black87,
+                  ),
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.all(11.0),
+                    child: Center(
+                      child: const Text(
+                        '시작하기',
+                        style: TextStyle(fontSize: 30, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/workout_list_page.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class WorkoutListPage extends StatelessWidget {
+  const WorkoutListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
