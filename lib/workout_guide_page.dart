fc742403806fdb9ded3f747ed97ef9b9e64222ab
JH <modoo.jhy@gmail.com>
2026-07-27 11:54:58 +0900

    2026-07-27 11:54:58

---
+++ lib/workout_guide_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class WorkoutGuidePage extends StatelessWidget {
+  const WorkoutGuidePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Workout Guide'),
+        centerTitle: true,
+      ),
+      body:Column(
+        children: [
+          
+        ],
+      )
+    );
+  }
+}
