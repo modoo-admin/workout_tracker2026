a5c9055ba0788c74b1fe5a1ed3834b2bf326adcc
JH <modoo.jhy@gmail.com>
2026-07-31 13:54:53 +0900

    2026-07-31 13:54:53

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:workout_tracker_2026/main.dart';
+import 'package:workout_tracker_2026/main_didchange.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
