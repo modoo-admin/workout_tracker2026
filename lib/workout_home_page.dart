93038fd6f260f4b8fe05fa4ca40b5fc455f78da2
JH <modoo.jhy@gmail.com>
2026-08-03 13:58:37 +0900

    2026-08-03 13:58:37

--- lib/workout_home_page.dart
+++ lib/workout_home_page.dart
@@ -26,22 +26,20 @@ class _WorkoutHomePageState extends State<WorkoutHomePage> {
             Row(
               children: [
                 Expanded(
-                  child: Center(
-                    child: Text.rich(
-                      TextSpan(
-                        children: [
-                          TextSpan(text: '반가워요', style:textTheme.bodyLarge?.copyWith(
-                            fontWeight: FontWeight.w700,
-                          ) ),
-                          TextSpan(text: ' 건강을 위한 한 걸음\n',style: textTheme.bodyLarge?.copyWith(
-                            fontWeight: FontWeight.w700,
-                            color: colorScheme.primary,
-                          ),
-                          ),
-                          TextSpan(text: '오늘도 힘차게 운동을 해볼까요?\n'),
-                          TextSpan(text: '> 내 프로필'),
-                        ],
-                      ),
+                  child: Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(text: '반가워요', style:textTheme.bodyLarge?.copyWith(
+                          fontWeight: FontWeight.w700,
+                        ) ),
+                        TextSpan(text: ' 건강을 위한 한 걸음\n',style: textTheme.bodyLarge?.copyWith(
+                          fontWeight: FontWeight.w700,
+                          color: colorScheme.primary,
+                        ),
+                        ),
+                        TextSpan(text: '오늘도 힘차게 운동을 해볼까요?\n'),
+                        TextSpan(text: '> 내 프로필'),
+                      ],
                     ),
                   ),
                 ),
