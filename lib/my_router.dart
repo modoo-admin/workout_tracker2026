10786e9c6572e64dbc71a078b2277456fb37cfcd
JH <modoo.jhy@gmail.com>
2026-08-07 12:02:04 +0900

    2026-08-07 12:02:04

--- lib/my_router.dart
+++ lib/my_router.dart
@@ -1,5 +1,6 @@
 import 'package:go_router/go_router.dart';
 import 'package:workout_tracker_2026/landing_page.dart';
+import 'package:workout_tracker_2026/workout_home_page.dart';
 
 // GoRouter configuration
 final router = GoRouter(
@@ -7,6 +8,10 @@ final router = GoRouter(
     GoRoute(
       path: '/',
       builder: (context, state) => LandingPage(),
+    ),
+    GoRoute(
+      path: '/workout_home',
+      builder: (context, state) => WorkoutHomePage(),
     ),
   ],
 );
