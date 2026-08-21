5416d9f536a10b8cbb06c2e466e57445f590f977
JH <modoo.jhy@gmail.com>
2026-08-21 15:45:21 +0900

    2026-08-21 15:45:21

--- lib/firebase_auth_service.dart
+++ lib/firebase_auth_service.dart
@@ -12,11 +12,21 @@ class FirebaseAuthService {
     required String password,
     String? name,
   }) async {
+    String? errorMessage;
     try{
       await _auth.createUserWithEmailAndPassword(email: email, password: password);
       await _auth.currentUser?.updateDisplayName(name);
       await _auth.currentUser?.sendEmailVerification();
-    }catch(e){
+    } on FirebaseAuthException catch (error){
+      switch (error.code){
+        case 'weak-password':
+          errorMessage='취약 패스워드';
+        case 'email-already-in-use':
+          errorMessage='사용중인 이메일';
+        default:
+          errorMessage = error.message;
+      }
+    } catch(e){
       throw Exception('회원가입 에러:$e');
     }
 
