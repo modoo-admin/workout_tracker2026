6652d5e2e12f60e27f8b94792106fd203f8116c3
JH <modoo.jhy@gmail.com>
2026-08-28 14:51:59 +0900

    2026-08-28 14:51:59

---
+++ lib/services/firestore_service.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../models/my_workout.dart';
+
+class FirestoreService {
+  FirebaseFirestore _fs=FirebaseFirestore.instance;
+
+  Future<void> createMyWorkout(MyWorkout myWorkout) async{
+    try{
+
+    }catch(e){
+      throw Exception('db error:$e');
+    }
+  }
+  
+  Future<MyWorkout?> readMyWorkout(String workoutId) async {
+    try{
+
+    }catch(e){
+      throw Exception('db error:$e');
+    }
+    return null;
+  }
+
+  Future<void> updateMyWorkout(MyWorkout myWorkout) async{
+    try{
+
+    }catch(e){
+      throw Exception('db error:$e');
+    }
+  }
+
+  Future<void> deleteMyWorkout(String workoutId) async{
+    try{
+
+    }catch(e){
+      throw Exception('db error:$e');
+    }
+  }
+
+}
