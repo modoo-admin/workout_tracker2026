5efa5a7e13ac447cd5413fb8b4c35050f26aa8cb
JH <modoo.jhy@gmail.com>
2026-08-28 15:35:29 +0900

    2026-08-28 15:35:29

--- lib/logic/my_workout_provider.dart
+++ lib/logic/my_workout_provider.dart
@@ -1,12 +1,14 @@
 import 'dart:collection';
 
 import 'package:flutter/material.dart';
+import 'package:workout_tracker_2026/services/firebase_auth_service.dart';
 
 import '../models/my_workout.dart';
 import '../services/firestore_service.dart';
 
 class MyWorkoutProvider extends ChangeNotifier{
   final _firestoreService=FirestoreService();
+  final _auth=FirebaseAuthService();
   final List<MyWorkout> _workouts = [
     MyWorkout(
       name: '어깨 스트레칭',
@@ -25,6 +27,7 @@ class MyWorkoutProvider extends ChangeNotifier{
     return UnmodifiableListView(_workouts);
   }
   Future<void> addMyWorkout(MyWorkout myWorkout)async{
+    myWorkout.uid=_auth.user?.uid;
     await _firestoreService.createMyWorkout(myWorkout);
     _workouts.add(myWorkout);
     //db에 추가 기능
