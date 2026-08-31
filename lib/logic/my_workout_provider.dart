989e55134544517ae91108ae247cc22bcf32bbf9
JH <modoo.jhy@gmail.com>
2026-08-31 14:55:39 +0900

    2026-08-31 14:55:39

--- lib/logic/my_workout_provider.dart
+++ lib/logic/my_workout_provider.dart
@@ -6,37 +6,43 @@ import 'package:workout_tracker_2026/services/firebase_auth_service.dart';
 import '../models/my_workout.dart';
 import '../services/firestore_service.dart';
 
-class MyWorkoutProvider extends ChangeNotifier{
-  final _firestoreService=FirestoreService();
-  final _auth=FirebaseAuthService();
-  final List<MyWorkout> _workouts = [
-
-  ];
-  List<MyWorkout> get workouts{
+class MyWorkoutProvider extends ChangeNotifier {
+  final _firestoreService = FirestoreService();
+  final _auth = FirebaseAuthService();
+  final List<MyWorkout> _workouts = [];
+  List<MyWorkout> get workouts {
     return UnmodifiableListView(_workouts);
   }
-  Future<void> fetchAllMyWorkouts()async{
-    if(_auth.user == null) return;
-    final fetchedMyWorkouts=await _firestoreService.fetchAllMyWorkouts(uid: _auth.user!.uid,limit: 5);
+
+  Future<void> fetchAllMyWorkouts() async {
+    if (_auth.user == null) return;
+    final fetchedMyWorkouts = await _firestoreService.fetchAllMyWorkouts(
+      uid: _auth.user!.uid,
+      limit: 5,
+      lastWorkout: _workouts.lastOrNull,
+    );
     _workouts.addAll(fetchedMyWorkouts);
     notifyListeners();
   }
-  Future<void> addMyWorkout(MyWorkout myWorkout)async{
-    myWorkout.uid=_auth.user?.uid;
+
+  Future<void> addMyWorkout(MyWorkout myWorkout) async {
+    myWorkout.uid = _auth.user?.uid;
     await _firestoreService.createMyWorkout(myWorkout);
     _workouts.add(myWorkout);
     //db에 추가 기능
     notifyListeners();
   }
 
-  Future<void> deleteMyWorkout(int deleteIndex) async{
+  Future<void> deleteMyWorkout(int deleteIndex) async {
     _workouts.removeAt(deleteIndex);
     notifyListeners();
   }
 
-  Future<void> updateMyWorkout({required List<bool> isSelected, required int workoutIndex,}) async{
-    _workouts[workoutIndex].workoutDays=isSelected;
+  Future<void> updateMyWorkout({
+    required List<bool> isSelected,
+    required int workoutIndex,
+  }) async {
+    _workouts[workoutIndex].workoutDays = isSelected;
     notifyListeners();
   }
-
 }
